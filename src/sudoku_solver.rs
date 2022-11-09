f5d5cca189e59236f2946b9cdf36d0a63e124f8c
Ostap Tymchenko <ostap.tymchenko@gmail.com>
2022-11-09 09:35:32 -0500

    update-inter

--- src/sudoku_solver.rs
+++ src/sudoku_solver.rs
@@ -1,23 +1,31 @@
+// comfy tables for displaying, see post_board function
 use comfy_table::{modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, *};
+// random for generating a random number for a tile
 use rand::prelude::*;
+//HashSet is used in todo
 use std::collections::HashSet;
+//round for math in legal checker
+extern crate round;
+use round::{round, round_up, round_down};
+// from is used for conversion
+use std::convert::From;
 
 pub fn sudoku_solver_main() {
     println!("sudoku_solver.rs init");
 
     let board = [
-        [0, 0, 0, 0, 0, 9, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 4],
-        [0, 0, 7, 6, 4, 8, 0, 0, 2],
-        [0, 0, 2, 0, 5, 0, 8, 0, 0],
-        [0, 0, 1, 8, 0, 0, 0, 2, 3],
-        [7, 0, 4, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 3, 0, 0, 0, 4, 0],
-        [0, 0, 0, 0, 6, 0, 5, 0, 0],
-        [0, 2, 3, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 9, 0, 0, 0], // 01-09
+        [0, 0, 0, 0, 0, 0, 0, 0, 4], // 10-18
+        [0, 0, 7, 6, 4, 8, 0, 0, 2], // 19-27
+        [0, 0, 2, 0, 5, 0, 8, 0, 0], // 28-36
+        [0, 0, 1, 8, 0, 0, 0, 2, 3], // 46-45
+        [7, 0, 4, 0, 0, 0, 0, 0, 0], // 46-54
+        [0, 0, 0, 3, 0, 0, 0, 4, 0], // 55-63
+        [0, 0, 0, 0, 6, 0, 5, 0, 0], // 64-72
+        [0, 2, 3, 0, 1, 0, 0, 0, 0], // 73-81
     ];
 }
-
+// this takes a 9*9 array as board and prints it into a table
 fn post_board(board: [[u8; 9]; 9]) {
     let mut post = Table::new();
     for i in 0..9 {
@@ -29,30 +37,39 @@ fn post_board(board: [[u8; 9]; 9]) {
     println!("{post}");
 }
 
+// this is the initial that takes a board and asigns it a random value
+// the value is then checked for validity
 fn solve_board_brute_force_1(mut board: [[u8; 9]; 9]) -> [[u8; 9]; 9] {
+    let mut tile_pos:u8 = 0;
     for rows in &mut board {
         for mut tile in rows {
+            tile_pos += 1;
             *tile = rand::thread_rng().gen_range(1..=9);
-            check_legality(tile);
+            check_legality(tile, tile_pos);
         }
     }
     board
 }
 
-fn check_legality(tiles: &mut u8) -> u8 {
+fn check_legality(tile: &mut u8, tile_pos: u8) -> u8 {
+    let ftile_pos:f64 = tile_pos as f64;
+    let row:f64 = round_up(tile_pos.into(), 0);
+
+    let illegal_row: [u8; 9] = todo!();
+    let illegal_col: [u8; 9] = todo!();
+    let illegal_box: [u8; 9] = todo!();
+
     
-    let illegal_row = [5, 4, 4];
-    let illegal_box = [1, 1, 1];
 
     let mut illegal_combined  = illegal_row.into_iter().chain(illegal_box.into_iter());
 
-    if illegal_combined.any(|x| x == *tiles) {
-        *tiles = rand::thread_rng().gen_range(0..9);
-        check_legality(tiles);
+    if illegal_combined.any(|x| x == *tile) {
+        *tile = rand::thread_rng().gen_range(0..9);
+        check_legality(tile, tile_pos);
         println!("legal didn't pass")
     } else {
         println!("legal passed")
     }
     //return
-    *tiles
+    *tile
 }
