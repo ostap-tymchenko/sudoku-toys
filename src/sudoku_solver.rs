eed39411fd9c5e5b4f160759b78dfe95aa71bac1
Ostap Tymchenko <ostap.tymchenko@gmail.com>
2022-11-13 14:24:53 -0500

    somthing

--- src/sudoku_solver.rs
+++ src/sudoku_solver.rs
@@ -2,8 +2,6 @@
 use comfy_table::{modifiers::UTF8_ROUND_CORNERS, presets::UTF8_FULL, *};
 // random for generating a random number for a tile
 use rand::prelude::*;
-//HashSet is used in todo
-use std::collections::HashSet;
 
 enum BoxRange {
     TopLeft,
@@ -44,15 +42,21 @@ fn post_board(board: [[i32; 9]; 9]) {
     println!("{post}");
 }
 
-// this is the initial that takes a board and asigns it a random value
-// the value is then checked for validity
+// this is the initial that takes a board and asigns random valus to non zero tiles
+// the value is then checked for validity using check_legality
+
 fn solve_board_brute_force(mut board: [[i32; 9]; 9]) -> [[i32; 9]; 9] {
     let mut tile_pos:i32 = 0;
     for rows in &board {
         for mut tile in rows {
             tile_pos += 1;
-            *tile = rand::thread_rng().gen_range(1..10);
-            check_legality(&mut tile, tile_pos, board);
+            println!("old ={}", tile);
+            if *tile == 0 {
+                let tile = rand::thread_rng().gen_range(1..10);
+                println!("new ={}", tile);
+                check_legality(&mut tile, tile_pos, board);
+            }
+            
         }
     }
     post_board(board);
@@ -60,44 +64,9 @@ fn solve_board_brute_force(mut board: [[i32; 9]; 9]) -> [[i32; 9]; 9] {
 }
 
 fn check_legality(tile: &mut i32, tile_pos:i32, board: [[i32; 9]; 9]) -> i32 {
-    let tile_pos_f32 =f32::from(tile_pos as u8);
-    dbg!(tile_pos);
+    let tile_pos_f32 =f32::from(tile_pos as u8);    dbg!(tile_pos);
+    let row: [i32; 9] = board[[tile_pos_f32 / 9.0].iter().collect()];   dbg!(row);
+    let col: [i32; 9] = board[[tile_pos_f32 % 9.0].iter().collect()];   dbg!(col);
 
-    let row: [i32; 9] = board[[tile_pos_f32.into()/9.0].iter().collect()];
-    dbg!(row);
-
-    let col: [i32; 9] = board[[tile_pos_f32%9.0].iter().collect()];
-    dbg!(col);
-    
-    // let illegal_box: [i32; 9] = 
-    let x_cord = row.iter().map(|x| x/3);
-    let y_cord = col.iter().map(|x| x/3);
-
-    let xy_box = match (x_cord, y_cord) {
-        (0, 0) => BoxRange::TopLeft,
-        (0, 1) => BoxRange::TopMiddle,
-        (0, 2) => BoxRange::TopRight,
-        (1, 0) => BoxRange::MiddleLeft,
-        (1, 1) => BoxRange::MiddleMiddle,
-        (1, 2) => BoxRange::MiddleRight,
-        (2, 0) => BoxRange::BottomLeft,
-        (2, 1) => BoxRange::BottomMiddle,
-        (2, 2) => BoxRange::BottomRight,
-        _ => panic!("bad math"),
-    };
-
-    let illegal_combined: HashSet<_> = row
-        .into_iter()
-        .chain(col.into_iter())
-        .chain(xy_box.iter())
-        .collect();
-
-    if illegal_combined.into().any(|x| x == *tile) {
-        *tile = rand::thread_rng().gen_range(1..10);
-        check_legality(tile, tile_pos, board);
-        println!("legal didn't pass")
-    } else {
-        println!("legal passed")
-    }
     *tile
 }
