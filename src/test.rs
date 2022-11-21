bf2abbd20052f1a8b1db273e8639e8d2aa162b1b
ostap-tymchenko <ostap-tymchenko@pop-os.localdomain>
2022-11-20 22:34:53 -0500

    tiying up loose ends, testing git

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,22 @@
+use std::collections::HashSet;
+
 pub fn main () {
+    let digits: i32 = 123456789.parse();
+    let rows = "ABCDEFGHI";
+    let cols = digits;
     
+   fn cross (a:&str, b:&str) -> Vec<String> {
+        let mut squares = vec![];
+        for a_char in a.chars() {
+            for b_char in b.chars() {
+                squares.push(format!("{}{}", a_char, b_char));
+            }
+        }
+        squares
+    }
+
+    let squares = cross(&rows, &cols);
+    dbg!(digits, &squares); 
+    assert_eq!(81, (squares).len())
+    //let unitlist: HashSet::new(); 
 }
